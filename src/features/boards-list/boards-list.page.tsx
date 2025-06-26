635a13fa41b0f5a0c61a56813f28a1fa03a592d8
Evalion8 <alisultanovaeva@gmail.com>
2025-06-26 18:22:45 +0300

    feat: add mocks && workers & set up queries

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,11 +1,15 @@
+import { rqClient } from '@/shared/api/instance';
 import { ROUTES } from '@/shared/model/routes';
 import { Link, href } from 'react-router-dom';
 
 function BoardsListPage() {
+  const boardsQuery = rqClient.useQuery('get', '/boards');
   return (
     <div>
       <h1>Boards list</h1>
-      <Link to={href(ROUTES.BOARD, { boardId: '1' })}>Board 1</Link>
+      {boardsQuery.data?.map((board) => (
+        <Link to={href(ROUTES.BOARD, { boardId: board.id })}>{board.name}</Link>
+      ))}
     </div>
   );
 }
