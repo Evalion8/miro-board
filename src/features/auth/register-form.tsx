137144930286233577932f7306768ab9533dea9e
Evalion8 <alisultanovaeva@gmail.com>
2025-07-05 20:04:38 +0300

    feat: add vorm validation for register page

---
+++ src/features/auth/register-form.tsx
@@ -0,0 +1,69 @@
+import { Button } from '@/shared/ui/kit/button';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from '@/shared/ui/kit/form';
+import { Input } from '@/shared/ui/kit/input';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '../../../node_modules/@hookform/resolvers/zod/src/zod';
+
+const registerSchema = z.object({
+  email: z
+    .string({ required_error: 'Email is requiered!' })
+    .email('Incorrect email'),
+
+  password: z.string().min(6, 'Password must be at least 6 symbols'),
+  confirmPassword: z.string({ required_error: 'Password is required!' }),
+});
+
+export function RegisterForm() {
+  const form = useForm({
+    resolver: zodResolver(registerSchema),
+  });
+
+  const onSubmit = form.handleSubmit((data) => {
+    console.log(data);
+  });
+  return (
+    <Form {...form}>
+      <form className="flex flex-col gap-4" onSubmit={onSubmit}>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="admin@gmail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="******" type="password" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="mt-4">
+          Enter
+        </Button>
+      </form>
+    </Form>
+  );
+}
